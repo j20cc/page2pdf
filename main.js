22e8b86fcd63bca4569c944ad04f26a329231084
luke <luke_44@163.com>
2024-08-28 11:34:35 +0800

    test

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-const path = 'pdf/';
+const path = "pdf/";
 const options = {};
 const cfgs = [
-    {url:'https://www.baidu.com', path:'baidu.pdf'},
+  { url: "https://www.baidu.com", path: "baidu.pdf" },
+  { url: "https://www.google.com", path: "google.pdf" },
+  { url: "https://www.bing.com", path: "bing.pdf" },
 ];
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   for (let i = 0; i < cfgs.length; i++) {
-    await page.goto(cfgs[i].url, {waitUntil: 'networkidle2'});
-    await page.pdf({path: path+cfgs[i].path, format: 'A4', ...options});
+    await page.goto(cfgs[i].url, { waitUntil: "networkidle2" });
+    await page.pdf({ path: path + cfgs[i].path, format: "A4", ...options });
+
     //sleep 100ms
-    await new Promise(resolve => setTimeout(resolve, 100));
+    await new Promise((resolve) => setTimeout(resolve, 100));
   }
 
   await browser.close();
